588617ebf87776a2ac06d3b2ce13cc8a73fa0267
mehnaz <mehnazm406@gmail.com>
2025-06-02 22:16:51 +0530

    Update DSA.js

--- DSA.js
+++ DSA.js
@@ -7332,3 +7332,20 @@ function maxProductOfSubArr(nums) {
 console.log(maxProductOfSubArr([2, 3, -2, 4]));
 console.log(maxProductOfSubArr([-2, 0, -1]));
 // =====================================================================================
+function moveZeroes(nums) {
+  let j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[j] = nums[i];
+      j++;
+    }
+  }
+  for (let i = j; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+  return nums;
+}
+console.log(moveZeroes([0, 1, 0, 3, 12]));
+console.log(moveZeroes([0]));
+//time and space complexity:O(n),O(1)
+
